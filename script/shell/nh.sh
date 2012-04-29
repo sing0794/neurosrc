615ec854b8225225c55eb82978b00332a41fd17f
Ashish Singh <sing0794@gmail.com>
2012-04-29 09:47:48 -0500

    Update script/shell/nh.sh

--- script/shell/nh.sh
+++ script/shell/nh.sh
@@ -42,7 +42,7 @@ touch /neuro/neurosrc/script/hive/insertratsaverage.q
 
 #Execute permissions
 chmod a+x /neuro/neurosrc/script/shell/*
-chmod a+x /neuro/neurosrc/script/hive/*
+chmod 777 /neuro/neurosrc/script/hive/*
 
 #Run the job
 cd /neuro/tmp
